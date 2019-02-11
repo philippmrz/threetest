e075cccaa8ab2ba98e7ce4c2007a8fda2b537db1
Philipp M <phmrch@gmail.com>
2019-02-11 11:56:02 +0000

    Fitted canvas to window size

--- js/app.js
+++ js/app.js
@@ -6,30 +6,28 @@ animate();
 
 function init() {
 
-	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-	camera.position.z = 1;
+	camera = new THREE.PerspectiveCamera(50, 1, .1, 1000);
+	camera.position.z = 9;
 
 	scene = new THREE.Scene();
 
-	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+	geometry = new THREE.BoxGeometry(1, 1, 1);
 	material = new THREE.MeshNormalMaterial();
 
-	mesh = new THREE.Mesh( geometry, material );
-	scene.add( mesh );
+	mesh = new THREE.Mesh(geometry, material);
+	scene.add(mesh);
 
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild( renderer.domElement );
+	renderer = new THREE.WebGLRenderer({antialias: true});
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
 
 }
 
 function animate() {
 
-	requestAnimationFrame( animate );
-
-	mesh.rotation.x += 0.01;
-	mesh.rotation.y += 0.02;
-
-	renderer.render( scene, camera );
+	requestAnimationFrame(animate);
+    mesh.position.x += .05
+    if (mesh.position.x > 6) mesh.position.x = -6
+	renderer.render(scene, camera);
 
 }
